e3fb0f77775713167ec89dd2b7e411e72b7d96c6
monsteronfire <monsteronfire@gmail.com>
2016-08-14 09:43:10 +0800

    Wrap Home search field and button in form tag

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,13 +6,13 @@ var Home = React.createClass({
   render: function() {
     return (
       <div className='weather-form-wrapper' style={styles.patternBackground}>
-        <div className='col-sm-4 text-center'>
+        <form className='col-sm-4 text-center'>
           <h1>Enter a City and State</h1>
           <input className='form-control' placeholder='Suva, Fiji' style={styles.space} />
           <button type='button' className='btn btn-lg btn-success' style={styles.space}>
             Get Weather!
           </button>
-        </div>
+        </form>
       </div>
     )
   }
