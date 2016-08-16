53a3782cc36b7088c657ce06ccfb8595aab5a45e
monsteronfire <monsteronfire@gmail.com>
2016-08-16 08:57:00 +0800

    Add api helper

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,8 +1,36 @@
 var React = require('react');
 var ReactRouter = require('react-router');
 var styles = require('../styles');
+var getForecast = require('../utils/openWeatherMapHealpers.js');
 
 var Home = React.createClass({
+  getDefaultProps: function () {
+    return {
+      direction: 'column'
+    }
+  },
+
+  propTypes: {
+    direction: PropTypes.string
+  },
+
+  getInitialState: function () {
+    return {
+      city: ''
+    }
+  },
+
+  handleSubmitCity: function () {
+    console.log(this.state.city)
+    getForecast(this.state.city)
+  },
+
+  handleUpdateCity: function (e) {
+    this.setState({
+      city: e.target.value
+    })
+  },
+
   render: function() {
     return (
       <div className='weather-form-wrapper' style={styles.patternBackground}>
--- app/utils/openWeatherMapHelpers.js
+++ app/utils/openWeatherMapHelpers.js
@@ -1,5 +1,49 @@
 var axios = require('axios');
 
-var id = 'YOUR_CLIENT_ID';
-var sec = 'YOUR_SECRET_ID';
-var param = '?client_id=' + id + '&client_secret=' + sec;
+var _baseURL = 'http://api.openweathermap.org/data/2.5/';
+var _APIKEY = '49ae324a5955a4833fedecd7c7285d79';
+
+function prepRouteParams (queryStringData) {
+  return Object.keys(queryStringData)
+    .map(function (key) {
+      return key + '=' + encodeURIComponent(queryStringData[key]);
+    }).join('&')
+}
+
+function prepUrl (type, queryStringData) {
+  return _baseURL + type + '?' + prepRouteParams(queryStringData);
+}
+
+function getQueryStringData (city) {
+  return {
+    q: city,
+    type: 'accurate',
+    APPID: _APIKEY,
+    cnt: 5
+  }
+}
+
+function getCurrentWeather (city){
+  var queryStringData: getQueryStringData(city);
+  var url = prepUrl('weather', queryStringData);
+
+  return axios.get(url)
+    .then(function (currentWeatherData) {
+      console.log(currentWeatherData.data)
+    })
+}
+
+function getForecast (city) {
+  var queryStringData = getQueryStringData(city);
+  var url = prepUrl('forecast/daily', queryStringData)
+
+  return axios.get(url)
+    .then(function (forecastData) {
+      console.log(forecastData.data)
+    })
+}
+
+module.exports = {
+  getCurrentWeather: getCurrentWeather,
+  getForecast: getForecast
+};
